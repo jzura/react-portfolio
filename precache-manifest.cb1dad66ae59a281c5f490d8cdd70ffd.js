59a6cebb268e00b579f76cd7847502c827987840
Stitch <john@comteque.com.au>
2020-09-30 12:32:33 +0800

    Updates

---
+++ precache-manifest.cb1dad66ae59a281c5f490d8cdd70ffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f89c73af25866cea27bec54d397eef",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7be0e6b12ed84267c22e",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "20e38aea0b88c70ec741",
+    "url": "/react-portfolio/static/css/main.d29f7740.chunk.css"
+  },
+  {
+    "revision": "7be0e6b12ed84267c22e",
+    "url": "/react-portfolio/static/js/2.ee631702.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.ee631702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e38aea0b88c70ec741",
+    "url": "/react-portfolio/static/js/main.59777731.chunk.js"
+  },
+  {
+    "revision": "c690530a46d41b2200e5",
+    "url": "/react-portfolio/static/js/runtime-main.70058037.js"
+  }
+]);
